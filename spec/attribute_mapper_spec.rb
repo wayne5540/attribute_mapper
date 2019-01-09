4609cbededb44780c7f377b0ee81bf5069df9892
Wayne <wayne.5540@gmail.com>
2019-01-09 21:30:49 +0800

    Add basic spec

--- spec/attribute_mapper_spec.rb
+++ spec/attribute_mapper_spec.rb
@@ -3,7 +3,42 @@ RSpec.describe AttributeMapper do
     expect(AttributeMapper::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  class FooModel
+    include AttributeMapper
+    
+    mapping :test do
+      has_same_attributes :name
+      from :title, to: :suffix
+    end
+
+    def name
+      "Foo"
+    end
+
+    def title
+      "Mr."
+    end
+  end
+
+  describe "#mapping" do
+    specify do
+      expect(FooModel.new.mapping).to eq({
+        test: {
+          name: :name,
+          title: :suffix
+        }
+      })
+    end
+  end
+
+  describe "#test_attributes" do
+    specify do
+      model = FooModel.new
+
+      expect(model.test_attributes).to eq({
+        name: model.name,
+        suffix: model.title
+      }.as_json)
+    end
   end
 end
